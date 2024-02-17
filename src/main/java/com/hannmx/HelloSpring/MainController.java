9928aa3a05b61076330eb6aaed6aaa9094f504a5
mxkud <mxkudk@yandex.ru>
2024-02-17 22:12:23 +0300

    Hello Project

---
+++ src/main/java/com/hannmx/HelloSpring/MainController.java
@@ -0,0 +1,25 @@
+package com.hannmx.HelloSpring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/registration")
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping("/submit")
+    public String submitForm(@RequestParam String username, @RequestParam String password) {
+        // Обработка данных формы (например, сохранение в базу данных)
+        return "redirect:/"; // Перенаправление на главную страницу
+    }
+}
